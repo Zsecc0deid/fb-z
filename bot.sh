49d7c91956e9718c29671b5a6409a3d5ca73fa17
widhisec <39062772+widhisec@users.noreply.github.com>
2019-04-09 00:52:13 +0700

    Add files via upload

--- bot.sh
+++ bot.sh
@@ -1,27 +1,27 @@
-#!/bin/bash --utf8
+#!/usr/bin/bash
 # powered by Zsecc0de.id | plase not recode for script
+# update : 08/04/2019
+# telegram : @Mr_28dh8
 clear;
 RED='\033[0;31m'
 CYAN='\033[0;36m'
-WHITE=$(tput setaf 7) #PUTIH
+WHITE=$(tput setaf 7)
 GREEN=$(tput setaf 2)
 source "botfb.sh"
-function NAA(){
+#https://graph.facebook.com/me/friends?&access_token=
+NAA() {
                  curl -Gs "https://graph.facebook.com/v3.2/me/friends/?fields=email&access_token=$toke&limit=$total" | grep -Po '(?<="email":)[^,]*' | tr -d '[]}"'
 }
-   function bangsat(){
-   	                 njir=$(curl -X GET "https://graph.facebook.com/v3.0/me?fields=home.limit(100)&access_token=$TOKEN" -s)
-   	                 lol=$(echo $njir | grep -Po '(?<="id":)[^,]*' | tr -d '[]}"')
-   	                 printf "$lol\n"
-
+bangsat() {
+   	             njir=$(curl -X GET "https://graph.facebook.com/v3.0/me?fields=home.limit(100)&access_token=$TOKEN" -s)
+   	             lol=$(echo $njir | grep -Po '(?<="id":)[^,]*' | tr -d '[]}"')
+   	             printf "$lol\n"
 }
-function APASALAHKU()
-{
-              URL=`curl -s "https://graph.facebook.com/me/groups?access_token=$tox" | grep -Po '(?<="id":)[^,]*' | tr '""' "\n"`
-              printf "$URL" >> groups.txt
+apasalahku() {
+                URL=`curl -s "https://graph.facebook.com/me/groups?access_token=$tox" | grep -Po '(?<="id":)[^,]*' | tr '""' "\n"`
+                printf "$URL" >> groups.txt
 }
-function banner()
-{
+banner() {
 printf "
 \t$RED███████╗███████╗███████╗ ██████╗
 \t$RED╚══███╔╝██╔════╝██╔════╝██╔════╝
@@ -59,10 +59,10 @@ for i in $(cat $idX); do
 commend $access $idX $pesanz
 done
 elif [[ $ga == "4" ]]; then
-	read -p "list : " TARGED
-	echo -n "masukkan access_token :"; read -s tokex
-	for i in $(cat $TARGED); do
-	Zsecc0de $TARGED $tokex
+read -p "list : " TARGED
+echo -n "masukkan access_token :"; read -s tokex
+for i in $(cat $TARGED); do
+Zsecc0de-crew.id $TARGED $tokex
 done
 fi
      case $ga in
@@ -72,28 +72,26 @@ fi
      		;;
      	6 )
      	    read -s -p "masukkan access_token : " tox
-     	    APASALAHKU $tox
+     	    apasalahku $tox
      esac
-    if [[ $ga == "7" ]]; then
-      read -p "masukkan access_token : " toke
-      read -p "limit : " total
-      NAA $toke $total
-    fi
+if [[ $ga == "7" ]]; then
+read -p "masukkan access_token : " toke
+read -p "limit : " total
+NAA $toke $total
+fi
 }
-function frend(){
-	#https://graph.facebook.com/me/friends?&access_token=
-               gas=$(curl -Gs "https://graph.facebook.com/v3.2/100001396455823/friends?access_token=$toks&pretty=1&limit=$limit") 
-               z=$(echo $gas | grep -Po '(?<="id":)[^,]*' | tr -d '}]"')
-               printf "$z" > teman.txt
-               printf "succes tersimpan!\n"
+frend() {
+                  gas=$(curl -Gs "https://graph.facebook.com/v3.2/100001396455823/friends?access_token=$toks&pretty=1&limit=$limit") 
+                  z=$(echo $gas | grep -Po '(?<="id":)[^,]*' | tr -d '}]"')
+                  printf "$z" > teman.txt
+                  printf "succes tersimpan!\n"
 }
-function Zsecc0de()
-{
+Zsecc0de-crew.id() {
                   njo=$(curl -X POST "https://graph.facebook.com/me/friends/$i?method=delete&access_token=$tokex" -s)
                   if [[ $njo =~ "true" ]]; then
-                  	printf "succes unfrend \n"
+                  	 printf "succes unfrend \n"
                   elif [[ $njo =~ "error" ]]; then
-                  	printf "gagal\n"
+                  	 printf "gagal\n"
                   fi
 }
 menu
